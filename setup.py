67b1c0fcc704c4906fbb3f489e52a3314b73296c
Ihor Durnopianov <ihor.durnopianov@outlook.com>
2020-08-21 14:13:21 +0300

    Add bare-minimum setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Simple setup script."""
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="style_gan",
+    version="v2019-02-01",
+    description="PyTorch implementation of the StyleGAN Generator",
+    packages=find_packages(),
+    url="https://github.com/lernapparat/lernapparat",
+    author="Thomas Viehmann",
+    author_email="tv.github@beamnet.de",
+)
